aaaba7e8a05acdee7a8c473308076457eba3bb53
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-07 12:56:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const style={
+    color:'blue',
+     backgroundColor: 'green',
+     paddingLeft:'30px'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Blog heading='Good'author='John'></Blog>
+  <Blog heading='Better'author='Rony'></Blog>
+  <Blog heading='Best'author='Kelly'></Blog>
+  <Mobile></Mobile>
+  <LoadData></LoadData>
+      <div>
+        <article className='blog'>
+          I am learning react and so far I having so much fun in it.
+          <h2 style={style}>You should try it</h2>
+          <p style={{color:'white'}}>What if you fail sometimes?? Trying is the main thing</p>
+        </article>
+      </div>
     </div>
   );
 }
 
+
+function Blog(props){
+  return(
+    <div>
+      <p>You are doing it {props.heading}</p>
+      <p>Remember the name {props.author}</p>
+    </div>
+  )
+}
+function Mobile(){
+  const[count,setCount]=useState(100);
+  const handleButton=(e)=>setCount(count-10);
+  if(count===0){
+    <h3>'no more change'</h3>
+  }
+  return(
+    <div>
+      <h3>{count}</h3>
+      <button onClick={handleButton}>Battery down</button>
+    </div>
+  )
+}
+function LoadData(){
+  const[data,setData]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res=>res.json())
+    .then(data=>setData(data))
+  },[])
+  return(
+    <div>
+      {
+        data.map(singleData=><Data title={singleData.title} id={singleData.userId}></Data>)
+      }
+
+    </div>
+  )
+}
+function Data(props){
+  return(
+    <div>
+      <h1>{props.id}</h1>
+    <h3>{props.title}</h3>
+    </div>
+  )
+}
 export default App;
