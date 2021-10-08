0e3a5369e790a87c661f564548ad2e146b621022
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-08 12:42:42 +0600

    folder-made

--- src/App.js
+++ src/App.js
@@ -6,24 +6,35 @@ function App() {
   
   return (
     <div className="App">
-      <Countries></Countries>
+      {/* <Countries></Countries> */}
     </div>
   );
 }
-function Countries(){
-  const[countries,setCountries]=useState([]);
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=>setCountries(data))
-  },[])
- return(
-   <div>
-     <h2>Travelling around</h2>
-     <h4>Countries available:{countries.length}</h4>
-   </div>
- )
+// function Countries(){
+//   const[countries,setCountries]=useState([]);
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=>setCountries(data))
+//   },[])
+//  return(
+//    <div>
+//      <h2>Travelling around</h2>
+//      <h4>Countries available:{countries.length}</h4>
+//      {
+//        countries.map(country=><Country name={country.name} capital={country.capital}></Country>)
+//      }
+//    </div>
+//  )
 
-}
+// }
+// function Country(props){
+//   return(
+//     <div>
+//      <h3>Name:{props.name}</h3>
+//      <h6>Capital:{props.capital}</h6>
+//     </div>
+//   )
+// }
 
 export default App;
