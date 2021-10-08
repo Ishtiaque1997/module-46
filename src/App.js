3aa5bfb319cec8a8de617be2f816f173def02f13
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-08 13:13:49 +0600

    more-done-here

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Countries from './Components/Countries/Countries';
-import Header from './Components/Header/Header';
-
-
-
 function App() {
   
   return (
     <div className="App">
-      <Header></Header>
       <Countries></Countries>
       
     </div>
   );
 }
-// function Countries(){
-//   const[countries,setCountries]=useState([]);
-//   useEffect(()=>{
-//     fetch('https://restcountries.com/v3.1/all')
-//     .then(res=>res.json())
-//     .then(data=>setCountries(data))
-//   },[])
-//  return(
-//    <div>
-//      <h2>Travelling around</h2>
-//      <h4>Countries available:{countries.length}</h4>
-//      {
-//        countries.map(country=><Country name={country.name} capital={country.capital}></Country>)
-//      }
-//    </div>
-//  )
 
-// }
-// function Country(props){
-//   return(
-//     <div>
-//      <h3>Name:{props.name}</h3>
-//      <h6>Capital:{props.capital}</h6>
-//     </div>
-//   )
-// }
 
 export default App;
--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 const Countries = () => {
+ const[countries,setCountries]=useState([]);
+ useEffect( ()=>{
+  fetch('https://restcountries.com/v3.1/all')
+  .then(res=>res.json())
+  .then(data=>setCountries(data))
+ },[])
  return (
   <div>
-   <h1>This is countries in the world</h1>
+   <h1>Hello from countries:{countries.length}</h1>
+   
   </div>
  );
 };
