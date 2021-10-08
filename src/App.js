d542e1073a65718b26efeb0f9868816fffa0d84c
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-08 12:26:33 +0600

    started

--- src/App.js
+++ src/App.js
@@ -3,73 +3,27 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const style={
-    color:'blue',
-     backgroundColor: 'green',
-     paddingLeft:'30px'
-  }
+  
   return (
     <div className="App">
-      <Blog heading='Good'author='John'></Blog>
-  <Blog heading='Better'author='Rony'></Blog>
-  <Blog heading='Best'author='Kelly'></Blog>
-  <Mobile></Mobile>
-  <LoadData></LoadData>
-      <div>
-        <article className='blog'>
-          I am learning react and so far I having so much fun in it.
-          <h2 style={style}>You should try it</h2>
-          <p style={{color:'white'}}>What if you fail sometimes?? Trying is the main thing</p>
-        </article>
-      </div>
+      <Countries></Countries>
     </div>
   );
 }
-
-
-function Blog(props){
-  return(
-    <div>
-      <p>You are doing it {props.heading}</p>
-      <p>Remember the name {props.author}</p>
-    </div>
-  )
-}
-function Mobile(){
-  const[count,setCount]=useState(100);
-  const handleButton=(e)=>setCount(count-10);
-  if(count===0){
-    <h3>'no more change'</h3>
-  }
-  return(
-    <div>
-      <h3>{count}</h3>
-      <button onClick={handleButton}>Battery down</button>
-    </div>
-  )
-}
-function LoadData(){
-  const[data,setData]=useState([]);
+function Countries(){
+  const[countries,setCountries]=useState([]);
   useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://restcountries.com/v3.1/all')
     .then(res=>res.json())
-    .then(data=>setData(data))
+    .then(data=>setCountries(data))
   },[])
-  return(
-    <div>
-      {
-        data.map(singleData=><Data title={singleData.title} id={singleData.userId}></Data>)
-      }
+ return(
+   <div>
+     <h2>Travelling around</h2>
+     <h4>Countries available:{countries.length}</h4>
+   </div>
+ )
 
-    </div>
-  )
 }
-function Data(props){
-  return(
-    <div>
-      <h1>{props.id}</h1>
-    <h3>{props.title}</h3>
-    </div>
-  )
-}
+
 export default App;
